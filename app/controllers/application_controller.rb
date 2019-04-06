6d7bab23c39b0a08b7d16fecb3ceffcdde448841
grangerl330 <grangerl330@gmail.com>
2019-04-06 11:14:12 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :current_user
+  helper_method :current_user, :logged_in?
+
+  private
+    def current_user
+      @user ||= User.find(session[:user_id]) if logged_in?
+    end
+
+    def logged_in?
+      session[:user_id]
+    end
+
+    def require_login
+      return redirect_to root_path unless logged_in?
+    end
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,20 @@
 class Ride < ActiveRecord::Base
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    if self.user.tickets < attraction.tickets && self.user.height < attraction.min_height
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif self.user.tickets < attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif self.user.height < attraction.min_height
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      self.user.tickets -= attraction.tickets
+      self.user.nausea += attraction.nausea_rating
+      self.user.happiness += attraction.happiness_rating
+      self.user.save
+      "Thanks for riding the #{attraction.name}!"
+    end
+  end
 end
